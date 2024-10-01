01a03076c443af15a51f83f05b122f377f1f2d99
Ricardo J. Méndez <ricardo@numergent.com>
2024-10-01 17:41:35 +0200

    Cache using SQLite to avoid too many requests
    We are already getting rate-limited as it is, but this should reduce
    the frequency.

--- src/cache.ts
+++ src/cache.ts
@@ -1,17 +1,45 @@
 import { getProfile } from './api';
+import { Database } from 'bun:sqlite';
 
-const handleMap: Map<string, string> = new Map();
+const db = new Database('handles.sqlite', { create: true });
+db.exec('PRAGMA journal_mode = WAL;');
+db.query(
+	'create table if not exists handles \
+            (did text not null, \
+             handle text not null, \
+             primary key (did));'
+).run();
+
+const addHandleQuery = db.query(
+	'insert into handles (did, handle) values (?1, ?2);'
+);
+const getHandleQuery = db.query('select handle from handles where did = ?1;');
+
+// const handleMap: Map<string, string> = new Map();
+
+function addHandle(did: string, handle: string) {
+	try {
+		// We might get two attempts at updating the handle, so disregard that error
+		addHandleQuery.run(did, handle!);
+	} catch (e) {
+		console.error(`Error adding handle for ${did}`, e);
+	}
+}
 
 export async function getHandle(did: string): Promise<string | undefined> {
 	let handle: string | undefined = undefined;
-	if (handleMap.has(did)) {
-		handle = handleMap.get(did);
-		console.debug(`CACHE HIT: ${did} -> ${handle}`);
-	} else {
+
+	const dbHandle = getHandleQuery.get(did);
+	if (dbHandle) {
+		// @ts-ignore
+		handle = dbHandle.handle;
+		// console.debug(`Got handle from cache for ${did}: ${handle}`);
+	}
+	if (!handle) {
 		try {
 			const data = await getProfile(did);
 			handle = data.handle;
-			handleMap.set(did, handle!);
+			addHandle(did, handle!);
 		} catch (e) {
 			console.error(`Error getting handle for ${did}`, e);
 		}
